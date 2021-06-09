985aacfb83857fe30d8ac8450cb12a6249cd7f83
Erik Lund <dk107160@dhigroup.com>
2021-06-09 14:06:12 +0200

    example updated for running within a cluster

--- examples/simple/PodList.cs
+++ examples/simple/PodList.cs
@@ -7,11 +7,52 @@ namespace simple
     {
         private static void Main(string[] args)
         {
-            var config = KubernetesClientConfiguration.BuildDefaultConfig();
+            Console.WriteLine("Running in cluster");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            var config = KubernetesClientConfiguration.InClusterConfig();
+
             IKubernetes client = new Kubernetes(config);
             Console.WriteLine("Starting Request!");
 
-            var list = client.ListNamespacedPod("default");
+            var list = client.ListNamespacedPod("job-executions-sbx");
             foreach (var item in list.Items)
             {
                 Console.WriteLine(item.Metadata.Name);
